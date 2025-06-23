9ee6e229344943428520a49ec0e7b3979fe1c8d7
alv-around <alfmail@pm.me>
2025-06-24 01:14:08 +0200

    sepparate read vertial from horizontal lines

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,14 @@
   import NavLink from "$lib/navLink.svelte";
 </script>
 
-<div class="container min-h-screen mx-auto px-4 notepad-background">
-  <nav class="p-4 flex gap-16">
+<div class="container min-h-screen mx-auto px-4 vertical-red-line">
+  <nav class="flex gap-16">
     <h1><NavLink href="/">Home</NavLink></h1>
     <h1><NavLink href="/blog">Blog</NavLink></h1>
     <h1><NavLink href="/contact">Contact</NavLink></h1>
   </nav>
 
-  {@render children()}
+  <div class="notepad-background min-h-screen">
+    {@render children()}
+  </div>
 </div>
