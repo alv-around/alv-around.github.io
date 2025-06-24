76fb02df7e0771e75c28ab00d732a45183b6605c
alv-around <alfmail@pm.me>
2025-06-24 15:10:23 +0200

    format correctly navLinks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,17 @@
   import NavLink from "$lib/navLink.svelte";
 </script>
 
-<div class="container min-h-screen mx-auto px-4 vertical-red-line">
-  <nav class="flex gap-16">
-    <h1><NavLink href="/">Home</NavLink></h1>
-    <h1><NavLink href="/blog">Blog</NavLink></h1>
-    <h1><NavLink href="/contact">Contact</NavLink></h1>
-  </nav>
+<div class="grid grid-cols-[10%_90%]">
+  <div class="min-h-screen vertical-red-line"></div>
+  <div class="flex flex-col">
+    <nav class="flex flex-row place-items-center gap-16">
+      <h1><NavLink href="/">Home</NavLink></h1>
+      <h1><NavLink href="/blog">Blog</NavLink></h1>
+      <h1><NavLink href="/contact">Contact</NavLink></h1>
+    </nav>
 
-  <div class="notepad-background min-h-screen min-w-screen">
-    {@render children()}
+    <div class="notepad-background min-h-screen min-w-screen">
+      {@render children()}
+    </div>
   </div>
 </div>
