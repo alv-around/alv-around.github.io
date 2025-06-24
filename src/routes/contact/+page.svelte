06d5f4519b292d4d91f737660f4dbc9fe550782a
alv-around <alfmail@pm.me>
2025-06-24 16:13:00 +0200

    add gifs to undone sites

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,41 @@
-<h1>Contact</h1>
+<script lang="ts">
+  // No Svelte script logic needed for this direct embed
+</script>
+
+<div class="tenor-gif-wrapper">
+  <div
+    class="tenor-gif-embed"
+    data-postid="3142971822601075158"
+    data-share-method="host"
+    data-aspect-ratio="1"
+    data-width="100%"
+  >
+    <a
+      href="https://tenor.com/view/it%27s-not-done-marques-brownlee-it%27s-not-finished-unfinished-incomplete-gif-3142971822601075158"
+      >It&#39;S Not Done Marques Brownlee GIF</a
+    >
+    from
+    <a href="https://tenor.com/search/it%27s+not+done-gifs"
+      >It&#39;S Not Done GIFs</a
+    >
+  </div>
+  <script
+    type="text/javascript"
+    async
+    src="https://tenor.com/embed.js"
+  ></script>
+</div>
+
+<style>
+  /* Optional: Add some styling to control the GIF's maximum width or centering */
+  .tenor-gif-wrapper {
+    max-width: 500px; /* Example: Limit the max width of the GIF container */
+  }
+
+  /* You might also want to ensure the iframe scales correctly */
+  .tenor-gif-embed {
+    /* If there are any issues with responsiveness, you might need to adjust this.
+       Tenor's script usually handles this well, especially with data-width="100%" */
+    height: auto; /* Ensure height adjusts with width */
+  }
+</style>
