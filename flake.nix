b0edd5fbfed8ab1423c98f1dca67608b188718ae
alv-around <alfmail@pm.me>
2026-05-06 12:51:36 +0200

    add flake and prettierrc

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Svelte project development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_25
+          prettier
+          typescript-language-server
+          svelte-language-server
+        ];
+      };
+    });
+}
